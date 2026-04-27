a95d1c330625c5f2c67de291da23f8e0e0b93da8
Qasim <qjalal3636@gmail.com>
2026-04-27 22:15:20 +0500

    created a card in app-container and style it

--- tip-calculator/src/App.jsx
+++ tip-calculator/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 
 function App() {
+
+
    
 
   return (
     <div className='app-container'>
        <div className='header'>Tip Calc</div>
+       <div className='card'>
+        
+       </div>
     </div>
   )
 }
---
+++ tip-calculator/src/components/BillInput.jsx
@@ -0,0 +1,12 @@
+import './BillInput.css'
+
+export function BillInput() {
+  return (
+    <div>
+      <span>Bill Amount</span>
+      <input
+       placeholder='0.00'
+      />
+    </div>
+  )
+}
