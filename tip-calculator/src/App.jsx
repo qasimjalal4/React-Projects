59a4bbb414110758b44992627860ee32d32182e3
Qasim <qjalal3636@gmail.com>
2026-04-27 22:30:50 +0500

    add BillInput component with controlled state

--- tip-calculator/src/App.jsx
+++ tip-calculator/src/App.jsx
@@ -1,15 +1,17 @@
+import { BillInput } from './components/BillInput'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
-
+  const [bill,setBill] = useState(0);
    
 
   return (
     <div className='app-container'>
        <div className='header'>Tip Calc</div>
        <div className='card'>
-        
+        <BillInput bill={bill} setBill={setBill} />
        </div>
     </div>
   )
--- tip-calculator/src/components/BillInput.jsx
+++ tip-calculator/src/components/BillInput.jsx
@@ -1,10 +1,20 @@
 import './BillInput.css'
 
-export function BillInput() {
+export function BillInput({bill,setBill}) {
+
+ 
+
+  function handleBillChange(event) {
+    setBill(parseFloat(event.target.value) || 0)
+  }
+
   return (
-    <div>
+    <div className='input-container'>
       <span>Bill Amount</span>
       <input
+       onChange={handleBillChange}
+       value={bill}
+       className='input-box'
        placeholder='0.00'
       />
     </div>
