726e890ebfbec760ff1bc12c8b3c897a102afb4f
Qasim <qjalal3636@gmail.com>
2026-05-25 12:30:53 +0500

    added a Header component

--- Password-Generator/src/App.jsx
+++ Password-Generator/src/App.jsx
@@ -1,10 +1,12 @@
- import './App.css'
+import { Header } from './components/Header'
+import './App.css'
 
 function App() {
   
 
   return (
     <div className='app-container'>
+      <Header />
       
     </div>
   )
---
+++ Password-Generator/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+
+export function Header() {
+  return (
+    <div className='header'>
+     <h1
+      className='title'
+     >Password Generator
+     </h1>
+     <p>
+      Create strong, secure passwords instantly
+     </p>
+    </div>
+  )
+}
