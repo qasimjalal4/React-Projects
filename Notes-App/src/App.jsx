fe878be3d972a290d02b2ad65769984f7fa96fd7
Qasim <qjalal3636@gmail.com>
2026-05-26 16:51:03 +0500

    added a search bar

--- Notes-App/src/App.jsx
+++ Notes-App/src/App.jsx
@@ -1,13 +1,22 @@
 import { Header } from './components/Header'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
  
+  const [text,setText] = useState('');
 
   return (
-    <>
+    <div className='app-container'>
       <Header />
-    </>
+      <input
+       className='search-bar'
+       placeholder='Search your notes...'
+       type='text'
+       onChange={e => setText(e.target.value)}
+       value={text}
+      />
+    </div>
   )
 }
 
