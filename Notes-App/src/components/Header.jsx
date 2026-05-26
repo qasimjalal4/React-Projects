3a0955bcd0df8c0b5b21467d0b5f902e1f1b6dbe
Qasim <qjalal3636@gmail.com>
2026-05-26 15:46:55 +0500

    added Header component

---
+++ Notes-App/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import NoteImg from '../assets/Notes.png'
+import './Header.css'
+
+
+export function Header() {
+  return (
+    <div className='header'>
+      <div className='left-side'>
+        <img className='notes-image' src={NoteImg}  />
+        <span className='notes-count'>4 Notes</span>
+      </div>
+      <div className='middle'>
+        <span className='title'>My Notes</span>
+      </div>
+      <div className='right-side'>
+        <button className='new-note-button'>+ New Note</button>
+      </div>
+    </div>
+  )
+}
