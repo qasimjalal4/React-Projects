84042fe7b3200358601a80b9b9b412d32c281258
qasimjalal4 <qjalal2005@gmail.com>
2026-06-15 00:35:22 +0500

    change the list color

--- Router-App/src/Pages/About.jsx
+++ Router-App/src/Pages/About.jsx
@@ -6,7 +6,7 @@ export function About() {
       <h1 className="text-3xl font-bold text-gray-900 mb-8">About This Project</h1>
       <p className="text-lg text-gray-900 mb-4 leading-relaxed "> This is a simple 2-page React Router demo to learn the basics of client-side routing.</p>
       <p className="font-semibold text-gray-900 mb-4">What you will learn: </p>
-      <ul className="list-disc list-inside space-y-2 text-gray-600 mb-6">
+      <ul className="list-disc list-inside space-y-2 text-gray-800 mb-6">
        <li>How to set up BrowserRouter</li>
        <li>Creating Routes with Route component</li>
        <li>Using NavLink for navigation</li>
