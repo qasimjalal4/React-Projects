dcc90fb3611927c5eab9ca4f64293c152d267998
qasimjalal4 <qjalal2005@gmail.com>
2026-06-15 00:16:10 +0500

    move the data in home page to start instead of center

--- Router-App/src/Pages/Home.jsx
+++ Router-App/src/Pages/Home.jsx
@@ -2,14 +2,14 @@
 export function Home() {
   return (
     <>
-     <div className='mx-auto max-w-md mt-12 flex justify-center flex-col items-center'>
+     <div className='mx-auto max-w-md mt-12 flex justify-center flex-col items-start'>
        <h1 className="text-3xl font-bold text-gray-900 mb-8 text-center">
         Welcome to Home Page
        </h1>
-       <p className="text-lg text-gray-900 mb-4 leading-relaxed text-center">
+       <p className="text-lg text-gray-900 mb-4 leading-relaxed">
         This is a simple React Router demo showing how navigation works between pages.
        </p>
-       <p className="text-lg text-gray-900 mb-12 leading-relaxed text-center">
+       <p className="text-lg text-gray-900 mb-12 leading-relaxed">
         Click the "About" link in the navbar to navigate to About page.
        </p>
        <button className="bg-blue-500 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-600 transition ">
