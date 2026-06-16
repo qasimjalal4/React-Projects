fc40ce16e786d67bdcafd6fcdce9bee34d27cc1a
qasimjalal4 <qjalal2005@gmail.com>
2026-06-16 15:46:06 +0500

    add currency converter UI with inputs ,selectors and buttons

--- Currency-Converter/src/App.jsx
+++ Currency-Converter/src/App.jsx
@@ -1,11 +1,35 @@
+import CurrencySelector from "./components/CurrencySelector"
 
 function App() {
   
 
   return (
-    <>
-   
-    </>
+    <div className="bg-white w-96 p-8 rounded-xl shadow-md">
+      <h1 className="text-2xl font-bold text-center mb-8 pt-2">Currency Converter</h1>
+      <label>Amount</label>
+      <input
+       placeholder="Enter amount"
+       className="w-full p-2 mt-2 mb-3 rounded-md border border-[#ddd]"
+      />
+
+       <CurrencySelector />
+       <CurrencySelector />
+      <button
+       className="w-full p-2 mt-2 mb-3 border-none bg-[#10b981] text-white rounded-md cursor-pointer
+       hover:bg-[#059669] transition
+       "
+      >
+        Swap
+      </button>
+      <button
+       className="w-full p-2 mt-2 mb-3 border-none bg-[#2563eb] text-white rounded-md cursor-pointer
+       hover:bg-[#1d4ed8] transition
+       "
+      >
+        Convert
+      </button>
+      <p className="text-center mt-3 font-bold">Enter amount to convert</p>
+    </div>
   )
 }
 
---
+++ Currency-Converter/src/components/CurrencySelector.jsx
@@ -0,0 +1,16 @@
+
+
+const CurrencySelector = () => {
+  return (
+    <>
+     <label>From</label>
+     <select name="" id=""
+      className="w-full p-2 mt-2 mb-3 rounded-md border border-[#ddd]"
+     >
+      <option>2</option>
+     </select>
+    </>
+  )
+}
+
+export default CurrencySelector
