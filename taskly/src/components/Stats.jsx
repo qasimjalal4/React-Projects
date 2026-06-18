5e6c8b8c9272ae536bc5061dfe3f6a8f273eb52d
qasimjalal4 <qjalal2005@gmail.com>
2026-06-18 22:46:05 +0500

    change the TodoInput and Stats styling from css to tailwind and delete their css files

--- taskly/src/components/Stats.jsx
+++ taskly/src/components/Stats.jsx
@@ -1,20 +1,20 @@
-import './Stats.css'
+
 
 export function  Stats({total,completed,remaining}) {
 
   return (
-    <div className="stats-row">
-      <div className="stat-box">
-        <span className="stat-number">{total}</span>
-        <span className="stat-label">TOTAL</span>
+    <div className="w-full mt-7 flex justify-center items-center gap-14">
+      <div  className="flex flex-col bg-white py-3 px-7 rounded-lg shadow-md font-semibold">
+        <span className="text-lg">{total}</span>
+        <span className="text-gray-600">TOTAL</span>
       </div>
-      <div className="stat-box">
-        <span className="stat-number">{remaining}</span>
-        <span className="stat-label">REMAINING</span>
+      <div className="flex flex-col bg-white py-[10px] px-6 rounded-lg shadow-md font-semibold">
+        <span className="text-lg">{remaining}</span>
+        <span className="text-gray-600">REMAINING</span>
       </div>
-      <div className="stat-box">
-        <span className="stat-number">{completed}</span>
-        <span className="stat-label">COMPLETED</span>
+      <div className="flex flex-col bg-white py-3 px-7 rounded-lg shadow-md font-semibold">
+        <span className="text-lg">{completed}</span>
+        <span className="text-gray-600">COMPLETED</span>
       </div>
     </div>
   )
--- taskly/src/components/TodoInput.jsx
+++ taskly/src/components/TodoInput.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './TodoInput.css'
 
 export function TodoInput({todos,setTodos}) {
 
@@ -28,18 +27,26 @@ export function TodoInput({todos,setTodos}) {
 
   return (
     
-    <div className="input-container">
-       <div className='input-parent'>
-        <span className='input-icon'>+</span>
+    <div className="w-full flex mt-7 gap-5 items-center">
+       <div  className="flex flex-1 ml-9 border border-[#e0ded9] pl-3 rounded-lg items-center bg-white
+        focus-within:border-[#4f7c6b]
+        focus-within:ring-2
+        focus-within:ring-[#4f7c6b]/10  
+      ">
+        <span className="bg-white text-gray-500 text-xl ml-2 mr-3"
+        >+</span>
       <input 
-       className="todo-input"
+       className="outline-none flex-grow bg-white border-none text-black text-[15px] py-[15px] rounded-lg"
        onChange={saveInputText}
        value={inputText} 
        placeholder='Enter your new task'
        />
        </div>
        <button
-       className="add-button"
+        className="border-none bg-[#4f7c6b] text-white cursor-pointer
+       py-[7px] px-[26px] rounded-md mr-8 text-base font-semibold
+       hover:opacity-90 active:opacity-75 transition-all
+       "
        onClick={addTodo}    
       >Add</button>
   
