cf77bc6ccfefc9418a2f263a78445bf5d489df4e
qasimjalal4 <qjalal2005@gmail.com>
2026-07-09 16:04:43 +0500

    use fake useGithubUser and test loading message

--- Github_Profile_Finder/src/App.test.jsx
+++ Github_Profile_Finder/src/App.test.jsx
@@ -1,20 +1,40 @@
+import * as githubHook from "./Hooks/useGithubUser"
 import { beforeEach, describe, expect, test,vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
+ 
 
-
+vi.mock('./Hooks/useGithubUser', () => ({
+  useGithubUser: vi.fn()
+}))
 
 describe('App', () => {
 
-  let user;
 
-  beforeEach(() => {
-    render(<App />)
-    user = userEvent.setup()
-  })
+beforeEach(() => {
+  githubHook.useGithubUser.mockReturnValue({
+      user: null,
+      isLoading: false,
+      error: null
+    })
+})  
+
+ function setup() {
+
+  const user = userEvent.setup()
+
+  render(<App />)
+
+  return {
+    user
+  }
+ }
+
 
   test('renders the search textbox', () => {
+
+  setup()   
 
    const input = screen.getByRole('textbox')
    expect(input).toBeInTheDocument()
@@ -22,6 +42,8 @@ describe('App', () => {
   })
 
   test('renders the Search button', () => {
+
+    setup()
 
    const button = screen.getByRole('button', {
     name: 'Search'
@@ -33,11 +55,15 @@ describe('App', () => {
 
   test('renders default profile message', () => {
 
+    setup()
+
    expect(screen.getByText('Enter a Github username')).toBeInTheDocument()
   })
 
 
   test('allows the user to type in search box', async () => {
+
+    const {user} = setup()
 
     const input = screen.getByRole('textbox')
 
@@ -49,11 +75,10 @@ describe('App', () => {
 
   test('Showing alert message for empty input', async () => {
 
+    const {user} = setup()
+
     const alertSpy = vi.spyOn(window, 'alert').mockImplementation(() => {})
-
-    const input = screen.getByRole('textbox')
-
-    expect(input).toHaveValue('')
+ 
 
     const button = screen.getByRole('button', {
       name: 'Search'
@@ -69,4 +94,22 @@ describe('App', () => {
 
     alertSpy.mockRestore()
   })
+
+  test('shows loading message', () => {
+
+    githubHook.useGithubUser.mockReturnValue({
+      user: null,
+      isLoading: true,
+      error: null
+    })
+
+
+    setup()
+    
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument()
+  })
+
+
+  
 })
