d3b557b554045cf9c9294c89255e2b2b93c234b1
qasimjalal4 <qjalal2005@gmail.com>
2026-07-09 23:06:53 +0500

    nextButton will only  appear when option is selected

--- Quiz-App/src/components/NextButton.jsx
+++ Quiz-App/src/components/NextButton.jsx
@@ -1,20 +1,25 @@
-import { useQuiz } from "../context/QuizContext"
-
+import { useQuiz } from "../context/QuizContext";
 
 const NextButton = () => {
-
-  const {nextQuestion, currentQuestion, questions} = useQuiz()
+  const { nextQuestion, currentQuestion, questions, selectedAnswer } =
+    useQuiz();
 
   return (
-   <div className="w-full flex justify-center items-center"> 
-    <button
-     className="bg-indigo-600 text-white font-semibold text-lg w-2/3 mt-6 rounded-lg h-10
-     hover:opacity-90 active:opacity-80 transition-opacity
+    <div className="w-full flex justify-center items-center h-10 mt-[10px]  ">
+      {selectedAnswer !== null && (
+        <button
+          className="bg-indigo-600 text-white font-semibold text-lg w-2/3  rounded-lg  
+     hover:opacity-90 active:opacity-80 transition-opacity h-full 
      "
-     onClick={nextQuestion}
-    >{(currentQuestion + 1) >= questions.length ? 'See Results': 'Next Question'}</button>
-   </div> 
-  )
-}
+          onClick={nextQuestion}
+        >
+          {currentQuestion + 1 >= questions.length
+            ? "See Results"
+            : "Next Question"}
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default NextButton
+export default NextButton;
