a627a6a0f82070bac14709dfc23fdfef4f8bc998
qasimjalal4 <qjalal2005@gmail.com>
2026-07-10 16:04:34 +0500

    fix some bugs and use beforeEach and afterEach to make tests cleaner

--- Github_Profile_Finder/src/Hooks/useGithubUser.test.js
+++ Github_Profile_Finder/src/Hooks/useGithubUser.test.js
@@ -1,9 +1,17 @@
-import { describe, test, expect, vi } from "vitest";
+import { describe, test, expect, vi, beforeEach, afterEach } from "vitest";
 import { renderHook, waitFor } from "@testing-library/react";
 import { useGithubUser } from "./useGithubUser";
 
 
 describe('useGithubUser', () => {
+
+  beforeEach(() => {
+     vi.stubGlobal('fetch', vi.fn())
+  })
+
+  afterEach(() => {
+     vi.unstubAllGlobals()
+  })
 
   test('renders initial state', () => {
   
@@ -18,7 +26,7 @@ describe('useGithubUser', () => {
   })
 
   test('renders github user successfully', async () => {
-  vi.stubGlobal('fetch', vi.fn())
+  
 
   fetch.mockResolvedValue({
     ok: true,
@@ -36,13 +44,12 @@ describe('useGithubUser', () => {
     expect(result.current.error).toBeNull()
   })
 
-  vi.unstubAllGlobals()
+   
 })
 
 
 test("shows 'User Not Found!' when github returns 404",  async () => {
-
-  vi.stubGlobal("fetch", vi.fn())
+ 
 
   fetch.mockResolvedValue({
     ok: false,
@@ -52,30 +59,30 @@ test("shows 'User Not Found!' when github returns 404",  async () => {
   const {result} = renderHook(() => useGithubUser('Hello'))
 
   await waitFor(() => {
-    expect(result.current.name).toBeNull()
+    expect(result.current.user).toBeNull()
     expect(result.current.isLoading).toBe(false)
     expect(result.current.error).toBe('User Not Found!')
   })
 
-    vi.unstubAllGlobals()
+    
 })
 
 
   test('shows an error when fetch fails', async () => {
 
-    vi.stubGlobal("fetch", vi.fn())
+     
 
-    fetch.mockRejectedValue()
-     new Error('Network Error!')
+    fetch.mockRejectedValue(
+     new Error('Network Error!'))
 
      const {result} = renderHook(() => useGithubUser('octocat'))
 
      await waitFor(() => {
-      expect(result.current.name).toBeNull()
+      expect(result.current.user).toBeNull()
       expect(result.current.isLoading).toBe(false)
       expect(result.current.error).toBe('Network Error!')
      })
   })
 
-    vi.unstubAllGlobals()
+    
 })
