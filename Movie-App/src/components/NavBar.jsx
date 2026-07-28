e13f0a18813f260f8a294f2d8277fda662fba5cc
qasimjalal4 <qjalal2005@gmail.com>
2026-07-28 16:30:35 +0500

    created a navbar with active navigation links

--- Movie-App/src/components/NavBar.jsx
+++ Movie-App/src/components/NavBar.jsx
@@ -1,7 +1,33 @@
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
+
+  const navLinkClass = ({isActive}) => isActive ?  'text-red-500': 'text-white' 
+
+
   return (
-    <nav className="p-4 text-gray">NavBar</nav>
+    <nav className="w-full h-[88px] flex  justify-between items-center px-[72px] border-b-[1px] border-b-white shadow-sm">
+     <div className="text-[27px] font-bold">
+      <span>Movie</span><span className="text-red-500">Scope</span>
+     </div>
+     <ul className="list-none flex gap-14 text-[17px] font-semibold cursor-pointer">
+      <li>
+       <NavLink
+        className={navLinkClass}
+       to="/">
+        Home
+       </NavLink>
+      </li>
+      <li>
+       <NavLink 
+       className={navLinkClass}
+       to="/favourites">
+        Favourites
+       </NavLink>
+      </li>
+     </ul>
+
+    </nav>
   )
 }
 
