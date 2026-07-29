53134f8330ed9140a7603a9f0eb31d34a396e1e9
qasimjalal4 <qjalal2005@gmail.com>
2026-07-29 11:52:47 +0500

    add a ring and shadow around search box when focus

--- Movie-App/src/components/SearchBar.jsx
+++ Movie-App/src/components/SearchBar.jsx
@@ -3,7 +3,9 @@ const SearchBar = ({ query, setQuery}) => {
   return (
     <div className="w-full flex justify-center items-center h-20 gap-8">
       <input
-       className="w-1/2 outline-none px-6 h-[50px] py-3 rounded-full text-black "
+       className="w-1/2 outline-none border-none px-6 h-[50px] py-3 rounded-full text-black
+       focus:ring-2 focus:ring-red-500 focus:shadow-md focus:shadow-red-500
+       "
        placeholder="Search your movies..."
        onChange={e => setQuery(e.target.value)}
        value={query}
