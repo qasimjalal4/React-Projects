e2308cfad84ba017d7bb57aa7603e1f378878f94
qasimjalal4 <qjalal2005@gmail.com>
2026-08-08 00:05:07 +0500

    Tested QuizApp to check if it is rendering correct screen

---
+++ Quiz-App/src/components/QuizApp.test.jsx
@@ -0,0 +1,66 @@
+import { describe, expect, test, vi, } from "vitest";
+import QuizApp from './QuizApp'
+import { render } from "@testing-library/react";
+import { useQuiz } from "../context/QuizContext";
+
+vi.mock('../context/QuizContext', () => ({
+
+  useQuiz: vi.fn()
+}))
+
+vi.mock('./WelcomeScreen', () => ({
+  default: () => <div>WelcomeScreen</div>
+}))
+
+vi.mock('./QuizScreen', () => ({
+  default: () => <div>QuizScreen</div>
+}))
+
+vi.mock('./ResultScreen', () => ({
+  default: () => <div>ResultScreen</div>
+}))
+
+
+
+describe('Quiz App', () => {
+
+  test('renders WelcomeScreen when game status is welcome', () => {
+
+    useQuiz.mockReturnValue({
+      gameStatus: 'welcome'
+    })
+
+    render(<QuizApp />)
+
+    expect(screen.getByText('WelcomeScreen')).toBeInTheDocument()
+  })
+
+
+  test('renders QuizScreen when game status is playing', () => {
+
+    useQuiz.mockReturnValue({
+      gameStatus: 'playing'
+    })
+
+    render(<QuizApp />)
+
+    expect(screen.getByText('QuizScreen')).toBeInTheDocument()
+  })
+
+
+  
+
+
+  test('renders ResultScreen when game status is finished', () => {
+
+    useQuiz.mockReturnValue({
+      gameStatus: 'finished'
+    })
+
+    render(<QuizApp />)
+
+    expect(screen.getByText('ResultScreen')).toBeInTheDocument()
+
+
+  })
+})
